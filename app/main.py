3845c06f7d9c512e06449d938255d62dbcc9a282
quentin cailly <152685636+quentin-alt@users.noreply.github.com>
2025-11-20 18:21:28 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,27 +1,9 @@
 import streamlit as st
-from single_asset import run_single_asset_app
-from portfolio import run_portfolio_app
 
-def main():
-    st.set_page_config(
-        page_title="Quant Dashboard - Python / Git / Linux",
-        layout="wide"
-    )
+st.set_page_config(
+    page_title="Test Streamlit minimal",
+    layout="wide"
+)
 
-    st.title("Quant Dashboard – Python / Git / Linux")
-    st.markdown("Plateforme de **recherche quantitative** avec données financières (quasi) temps réel.")
-
-    # Sidebar : choix du module
-    st.sidebar.title("Navigation")
-    module = st.sidebar.radio(
-        "Choisir un module :",
-        ("Single Asset (Quant A)", "Portfolio (Quant B)")
-    )
-
-    if module == "Single Asset (Quant A)":
-        run_single_asset_app()
-    else:
-        run_portfolio_app()
-
-if __name__ == "__main__":
-    main()
+st.title("Hello Streamlit 👋")
+st.write("Si tu vois ce texte, l'exécution du script fonctionne.")
