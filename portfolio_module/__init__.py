9f1c7729b6ff3cceb91741489cdf08a42415faac
Camille170504 <camille.bouvier@edu.devinci.fr>
2026-01-08 15:21:38 +0100

    Delete portfolio_module directory

--- portfolio_module/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data import get_data
-from .portfolio import Portfolio
-from .metrics import calculate_metrics
-from .plots import plot_portfolio, plot_assets
--- portfolio_module/plots.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plot_assets(data):
-    data.plot(title="Prix des actifs")
-    plt.show()
-
-def plot_portfolio(cumulative_value):
-    cumulative_value.plot(title="Valeur cumulative du portefeuille")
-    plt.show()
